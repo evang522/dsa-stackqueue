b6ff5f548723353df01c52d4fbad56f2de1667ac
Evan Garrett <evang522@gmail.com>
2018-04-06 17:12:35 -0400

    Finished Syntax validator

--- Stack.js
+++ Stack.js
@@ -34,12 +34,12 @@ class Stack {
 
   pop() {
     if (this.top === null) {
-      return console.warn('This list is empty');
+      return false;
     }
 
     const node = this.top;
     this.top = node.next;
-    return node.value;
+    return node;
   }
   
   
@@ -72,21 +72,74 @@ const display = stack => {
 };
 
 
-const is_palindrome = str => {
+//============================================ Check for Palindromes ======================================>
+
+const isPalindrome = str => {
   str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+  const stringStack = new Stack();
+
+  for (let i = 0; i<str.length;i++) {
+    stringStack.push(str[i]);
+  }
+
+  let result = true;
+  for (let i = 0; i < str.length; i++) {
+    if (stringStack.pop().value !== str[i]) {
+      result = false;
+    }
+  }
+
+  return result;
+
 };
+
+//============================================ Check for Errors in Syntax ======================================>
+
+// Arithmetic expression= '(3+3) + 5 - ((2+3)'
+
+// use a for loop to loop through the expression. 
+// Every time there is an open parentheses, push an object containing the openData type and the index with which it started.
+// Every time there is a close parentheses, pop the most recent parentheses in the stack
+// Once the for loop is finished, if stack.pop() === false, then return 'valid expression'. if this function returns something, return `Unclosed Parentheses at this index`
+
+const validSyntax = str => {
+  const syntaxStack = new Stack();
+  for (let i=0; i<str.length;i++) {
+    if (str[i] === '(') {
+      syntaxStack.push({index:i});
+    } else if (str[i] === ')') {
+      syntaxStack.pop();
+    } else {
+      continue;
+    }
+  }
+
+
+  let openParens = syntaxStack.pop().value;
+  if (!openParens) {
+    return 'Your expression has valid syntax';
+  } else {
+    return `Unclosed parenthesis at index ${openParens.index}`;
+  }
+};
+
+
+
 
 //============================================ Main Function for Implementation ======================================>
 
 function main() {
   const starTrek = new Stack();
-  starTrek.push('Kirk');
-  starTrek.push('Spock');
-  starTrek.push('Scotty');
-  starTrek.push('McCoy');
-  display(starTrek);
-  starTrek.pop();
-  display(starTrek);
+  // starTrek.push('Kirk');
+  // starTrek.push('Spock');
+  // starTrek.push('Scotty');
+  // starTrek.push('McCoy');
+  // display(starTrek);
+  // starTrek.pop();
+  // display(starTrek);
+  // console.log(isPalindrome('a man! a plann! a canal! panama!'));
+  console.log(validSyntax('(3+3) + 5 - (2+3)'));
 }
 
 main();

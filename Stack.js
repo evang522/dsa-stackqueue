41dc9e349f78369ca9d45f2c4cc3a1937df6ce66
Evan Garrett <evang522@gmail.com>
2018-04-07 15:59:45 -0400

    Stack sort complete

--- Stack.js
+++ Stack.js
@@ -28,6 +28,7 @@ class Stack {
 
     const node = this.top;
     this.top = new _Node(value,node);
+    // console.log('this is the TOP: ', this.top);
     return this.top;
   }
 
@@ -40,6 +41,13 @@ class Stack {
     const node = this.top;
     this.top = node.next;
     return node;
+  }
+
+  peek() {
+    if (this.top === null) {
+      return false;
+    }
+    return this.top;
   }
   
   
@@ -126,11 +134,86 @@ const validSyntax = str => {
 
 
 
+//============================================ Sort Stack ======================================>
+
+// algorithm takes in a stack with numbers;
+// create a new stack
+// loop through the current stack
+//  each iteration, pop the value off of the top of the original stack & compare it with
+// the peek value of the new stack.
+// If the new stack's value is greater, just insert the new value into the new stack.
+// If the original stack's value is greater, pop off the new stack's top value and insert the
+// value that was just popped off of the original stack, then re-insert the new stack's value that
+// was removed. 
+// Return the new stack.
+
+
+// const sortStack = givenStack => {
+//   const newStack = new Stack();
+//   while (givenStack.top !== null) {
+
+//     let oldStackNode = givenStack.pop();
+
+//     if (newStack.peek()) {
+//       if (newStack.peek().value >= oldStackNode.value ) {
+//         newStack.push(oldStackNode.value);
+//       } else {
+//         let newStackNode = newStack.pop();
+//         newStack.push(oldStackNode.value);
+//         newStack.push(newStackNode.value);
+//       }
+//     } else {
+//       newStack.push(oldStackNode.value);
+//     }
+
+//   }
+//   return newStack;
+// };
+
+
+// OK So none of that worked. Let's try again
+
+/**
+ * SortStack: Sorts a given stack class according  ascending numerically
+ * @param: stack {Stack} Stack to sort
+ * @returns: stack {Stack} sorted Stack
+ */
+
+// Take in stack
+// create temporary stack;
+// while the current stack is not empty:
+//  pop the current stack's top value and store it as TMP
+//  while the temporary stack is not empty and it's top value is less than TMP
+//    pop off the temp stack's value and push it to the original stack
+//  push TMP to the temp stack
+//  return the temporary stack
+
+const stackSort = stack => {
+  const newStack = new Stack();
+
+  while (stack.top !== null) {
+
+    const tempNode = stack.pop();
+
+    while (newStack.top !== null && newStack.peek().value < tempNode.value) {
+      stack.push(newStack.pop().value);
+    }
+    newStack.push(tempNode.value);
+
+  }
+
+  return newStack;
+};
+
+
+
+
+
 
 //============================================ Main Function for Implementation ======================================>
 
 function main() {
-  const starTrek = new Stack();
+  // const starTrek = new Stack();
   // starTrek.push('Kirk');
   // starTrek.push('Spock');
   // starTrek.push('Scotty');
@@ -139,10 +222,21 @@ function main() {
   // starTrek.pop();
   // display(starTrek);
   // console.log(isPalindrome('a man! a plann! a canal! panama!'));
-  console.log(validSyntax('(3+3) + 5 - (2+3)'));
+  // console.log(validSyntax('(3+3) + 5 - (2+3)'));
+
+  const ssNode = new Stack();
+  ssNode.push(21);
+  ssNode.push(955);
+  ssNode.push(23);
+  ssNode.push(-542);
+  ssNode.push(7);
+  ssNode.push(1430);
+  ssNode.push(0);
+  console.log('Original Stack: ');
+  display(ssNode);
+  console.log('New Stack');
+  let newStack = stackSort(ssNode);
+  display(newStack);
 }
 
 main();
-
-
-
